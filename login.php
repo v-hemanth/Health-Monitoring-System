46c07565b0497cf4a3da51a6bdbf9e1568b60538
Hemanth <76104612+v-hemanth@users.noreply.github.com>
2021-08-23 16:49:25 +0530

    Add files via upload

---
+++ login.php
@@ -0,0 +1,39 @@
+<?php
+  define('db_server','localhost');
+  define('db_username','root');
+  define('db_password','');
+  define('db_database','health tracker');
+  $connection=mysqli_connect(db_server,db_username,db_password,db_database);
+  if($connection){
+      echo"Succeccfully connected to database <br>";
+
+      
+   if(isset($_POST['uname']) and isset($_POST['pass'])){
+    $username=$_POST['uname'];
+    $password=$_POST['pass'];
+    
+    $query="SELECT * FROM usertable WHERE uname='$username' and pass='$password'";
+    
+    $result=mysqli_query($connection,$query) or die(mysqli_error($connection));
+    
+    $count=mysqli_num_rows($result);
+    
+    if($count==1)
+    {
+        $_SESSION['username']=$username;
+        echo"You have logged in successfully";
+        
+        header("location: uhome.html");
+    }
+    else{
+        $fmsg="Invalid username or password";
+        echo"<br>Invalid username or password";
+        
+    }
+  }
+  }
+else{
+    echo"Failed to connect to database";
+   }
+  
+?>
